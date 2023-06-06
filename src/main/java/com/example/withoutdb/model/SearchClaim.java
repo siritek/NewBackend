d2d46df7aaeaa50fb26cb806f462e9fc72036c2d
VishalS134 <vsai@nxtleveltech.com>
2023-06-07 02:26:05 +0530

    SearchClaim

---
+++ src/main/java/com/example/withoutdb/model/SearchClaim.java
@@ -0,0 +1,159 @@
+package com.example.withoutdb.model;
+
+public class SearchClaim {
+
+
+    private String ClaimNumber;
+    private String policyNumber;
+    private String SearchFor;
+
+    private String FirstName;
+
+    private String LastName;
+
+    private String OrganizationName;
+
+    private String TaxID;
+
+    private String Insured;
+
+    private String Claimant;
+
+    private String LossDate;
+
+    private String Adjuster;
+
+    private String Status;
+
+    private String RemainingReserves;
+
+    private String FuturePayments;
+
+    private String Paid;
+
+
+
+    public String getClaimNumber() {
+        return ClaimNumber;
+    }
+
+    public void setClaimNumber(String claimNumber) {
+        ClaimNumber = claimNumber;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getSearchFor() {
+        return SearchFor;
+    }
+
+    public void setSearchFor(String searchFor) {
+        SearchFor = searchFor;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getOrganizationName() {
+        return OrganizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        OrganizationName = organizationName;
+    }
+
+    public String getTaxID() {
+        return TaxID;
+    }
+
+    public void setTaxID(String taxID) {
+        TaxID = taxID;
+    }
+
+    public String getInsured() {
+        return Insured;
+    }
+
+    public void setInsured(String insured) {
+        Insured = insured;
+    }
+
+    public String getClaimant() {
+        return Claimant;
+    }
+
+    public void setClaimant(String claimant) {
+        Claimant = claimant;
+    }
+
+    public String getLossDate() {
+        return LossDate;
+    }
+
+    public void setLossDate(String lossDate) {
+        LossDate = lossDate;
+    }
+
+    public String getAdjuster() {
+        return Adjuster;
+    }
+
+    public void setAdjuster(String adjuster) {
+        Adjuster = adjuster;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getRemainingReserves() {
+        return RemainingReserves;
+    }
+
+    public void setRemainingReserves(String remainingReserves) {
+        RemainingReserves = remainingReserves;
+    }
+
+    public String getFuturePayments() {
+        return FuturePayments;
+    }
+
+    public void setFuturePayments(String futurePayments) {
+        FuturePayments = futurePayments;
+    }
+
+    public String getPaid() {
+        return Paid;
+    }
+
+    public void setPaid(String paid) {
+        Paid = paid;
+    }
+
+
+
+
+}
