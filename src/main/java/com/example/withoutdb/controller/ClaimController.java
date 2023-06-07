4cd7a09a1432b935b2f5190909e0e8310e9c45cc
suryarao64 <suryarao64@gmail.com>
2023-06-07 16:19:52 -0500

    code:- Cors config and ClaimantType,Lossparty,PolicyStatus,PolicyType DAO and controller class  Dropdown values from DB.

--- src/main/java/com/example/withoutdb/controller/ClaimController.java
+++ src/main/java/com/example/withoutdb/controller/ClaimController.java
@@ -18,7 +18,6 @@ public class ClaimController {
     public ClaimController(DBConn dbConn) {
         this.dbConn = dbConn;
     }
-
     public static class FnolWrapper {
         private List<Fnol> claimloss;
         public FnolWrapper(List<Fnol> claimloss) {
@@ -32,22 +31,53 @@ public class ClaimController {
             this.claimloss = claimloss;
         }
     }
-
     @GetMapping("/loss/{policy}")
     public Fnol createLoss(@PathVariable("policy") String policy) {
         Fnol claimLoss = new Fnol();
+        System.out.println(policy);
 
         try {
             Connection conn = dbConn.getMyConnection();
-            PreparedStatement statement = conn.prepareStatement("SELECT reportedby, FROM fnol WHERE policynumber = ?");
+            PreparedStatement statement = conn.prepareStatement("SELECT reportedby,description,loss_cause,incident_only,coverage_in_question,how_reported,first_suit_notice,weather_related,catastrophe FROM claim WHERE policynumber = ?");
             statement.setString(1, policy);
-
 
             ResultSet rs = statement.executeQuery();
             if (rs.next()) {
+                //policynumber,description, loss_cause,incident_only, coverage_in_question, how_reported, first_suit_notice, weather_related, catastrophe
                 claimLoss.setReportedBy(rs.getString("reportedby"));
+                claimLoss.setDescription(rs.getString("description"));
+                claimLoss.setLoss_cause(rs.getString("loss_cause"));
+                claimLoss.setIncident_only(rs.getBoolean("incident_only"));
+                claimLoss.setCoverage_in_question(rs.getBoolean("coverage_in_question"));
+                claimLoss.setHow_reported(rs.getString("how_reported"));
+                claimLoss.setFirst_suit_notice(rs.getBoolean("first_suit_notice"));
+                claimLoss.setWeather_related(rs.getBoolean("weather_related"));
+                // claimLoss.setCatastrophe(rs.getString("catastrophe"));//catastrophe
+                // add date dated to send to CC
+            }
 
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Exception in createLoss method: " + e);
+        }
+        return claimLoss;
+    }
 
+}
+
+    /*@GetMapping("/loss/{policy}")
+    public ResponseEntity<FnolWrapper> createLoss(@PathVariable("policy") String policy) {
+        List<Fnol> claimloss = new ArrayList<>();
+
+        try {
+            Connection conn = dbConn.getMyConnection();
+            PreparedStatement statement = conn.prepareStatement("SELECT reportedby FROM fnol WHERE policynumber = ?");
+            statement.setString(1, policy);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Fnol x = new Fnol();
+                x.setReportedby(rs.getString("reportedby"));
+                claimloss.add(x);
             }
 
             conn.close();
@@ -55,5 +85,68 @@ public class ClaimController {
             System.out.println("Exception in createLoss method: " + e);
         }
 
-        return claimLoss;
-    }}
+        FnolWrapper response = new FnolWrapper(claimloss);
+        return ResponseEntity.ok(response);
+    }
+}*/
+/*
+
+
+    @GetMapping("/loss/{policy}")
+    public List<Fnol> createLoss(@PathVariable("policy") String policy) {
+        List<Fnol> claimloss = new ArrayList<>();
+
+        try {
+            Connection conn = dbConn.getMyConnection();
+            PreparedStatement statement = conn.prepareStatement("SELECT reportedby FROM fnol WHERE policynumber = ?");
+            statement.setString(1, policy);
+
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Fnol x = new Fnol();
+                x.setReportedby(rs.getString("reportedby"));
+                claimloss.add(x);
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Exception in createLoss method: " + e);
+        }
+
+        return claimloss;
+    }
+}  */
+/*
+
+    @GetMapping("/loss/{policy}")
+    public List<Fnol> createLoss( @PathVariable("policy") String policy) {
+
+        List<Fnol> claimloss = new ArrayList<Fnol>();
+        try {
+            Connection conn = dbConn.getMyConnection();
+            PreparedStatement statement = conn.prepareStatement("SELECT reportedby FROM fnol WHERE policynumber = ?");
+            statement.setString(1, policy);
+            System.out.println(""+Integer.parseInt(String.valueOf(policy))+" test ");
+            conn.close();
+
+
+        } catch (SQLException e) {
+            System.out.println("Exception in getFnol method: " + e);
+        }
+
+        // Create a new list with only claimnumber and timeofloss properties
+        List<Fnol> result = new ArrayList<>();
+        for (Fnol fnol : claimloss) {
+            Fnol newFnol = new Fnol();
+            newFnol.setReportedby(fnol.getReportedby());
+            //  newFnol.setClaimnumber(fnol.getClaimnumber());
+            //  newFnol.setTimeofloss(fnol.getTimeofloss());
+            result.add(newFnol);
+        }
+
+        return result;
+    }
+
+
+}
+*/
